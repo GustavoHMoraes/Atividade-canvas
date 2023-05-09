257e4ada00976b79db2b0a011af7269dc15adbac
GustavoHMoraes <gustavo.rocillo@etec.sp.gov.br>
2023-05-09 09:22:30 -0300

    atualizacao

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 const canvas = document.getElementById('ret');
 const ctx = canvas.getContext('2d');
- ctx.fillStyle="green";
- ctx.fillRect(10,10,150,100);
+ ctx.fillStyle="#2ACAEA";
+ ctx.fillRect(0,0,250,400);
+ ctx.fillStyle="#cff1c5";
+ ctx.fillRect(250,0,250,400);
 
- var c = document.getElementById("MyCanvas");
+ var c = document.getElementById("circ");
  var ctxc = c.getContext('2d');
  ctxc.beginPath();
- ctxc.arc(95, 50, 40, 0, 2 * Math.PI);
+ ctxc.arc(250, 150, 140, 0, 2 * Math.PI);
+ ctxc.lineWidth = 20;
+ ctxc.strokeStyle="#3C0A51"
  ctxc.stroke();
- ctxc.fillStyle = 'orange';
-ctxc.fillStyle = '#FFA500';
-ctxc.fillStyle = 'rgb(255, 165, 0)';
-ctxc.fillStyle = 'rgba(255, 165, 0, 1)';
+ ctxc.fillStyle="green"
+ ctxc.fill();
